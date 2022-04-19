28706a1f97349a56541e88786a2794093fa19eee
quentin <quentin.wegera.wegera@mmibordeaux.com>
2022-04-19 18:46:28 +0200

    css ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/login'
+  # get 'sessions/welcome'
+  # get 'users/new'
+  # get 'users/create'
+
   resources :places
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -8,4 +15,11 @@ Rails.application.routes.draw do
   #get '/home', to: 'pages#home'
   get '/home', to: 'pages#home'
   get '/index', to: 'places#index'
+
+  resources :users, only: [:new, :create]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
+
 end
